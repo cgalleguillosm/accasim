84bf4ce316b22c78dadea531cc431e43285d849f
cgalleguillosm <cristian.galleguillos.m@mail.pucv.cl>
2017-07-08 18:46:57 +0200

    Basic example corrected.

--- basic_example.py
+++ basic_example.py
@@ -25,11 +25,9 @@ from accasim.base.simulator_class import hpc_simulator
 from accasim.base.scheduler_class import fifo_sched
 from accasim.base.allocator_class import ffp_alloc
 
-# workload = 'workloads/HPC2N-2002-2.2.1-cln.swf'
-workload = 'workloads/workload.swf'
+workload = 'workloads/HPC2N-2002-2.2.1-cln.swf'
 sys_cfg = 'config/HPC2N.config'
 
-_seed = 'example'
 allocator = ffp_alloc()
 dispatcher = fifo_sched(allocator)
 simulator = hpc_simulator(sys_cfg, workload, dispatcher, RESULTS_FOLDER_NAME='results') 
