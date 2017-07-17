42353b615d1368fe2bfa2013b80e7914b1938fd8
cgalleguillosm <cristian.galleguillos.m@mail.pucv.cl>
2017-07-17 16:36:51 +0200

    Relative path of accasim for autogen by readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('D:\\Programas\\git\\accasim'))
+sys.path.insert(0, '../../accasim')
 
 
 # -- General configuration ------------------------------------------------
