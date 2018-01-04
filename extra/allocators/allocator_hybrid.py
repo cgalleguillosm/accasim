4af60d909fc07607ef4a05c94dbee33de128ec20
Alessio Netti <netti.alessio@gmail.com>
2018-01-04 15:29:46 +0100

    Import fix for Hybrid allocator

--- extra/allocators/allocator_hybrid.py
+++ extra/allocators/allocator_hybrid.py
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-from accasim.base.allocator_weighted import allocator_weighted
+from extra.allocators.allocator_weighted import allocator_weighted
 from accasim.base.allocator_class import ffp_alloc
 
 
