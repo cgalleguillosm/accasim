bc4edcf7210830622e9a2cb86abf3e55ca93587d
cgalleguillosm <cristian.galleguillos.m@mail.pucv.cl>
2018-05-28 15:01:28 +0200

    JobVerification
    NO_CHECK by default. Any data inconsistency must be fixed by the
    Dispatcher

--- accasim/base/scheduler_class.py
+++ accasim/base/scheduler_class.py
@@ -55,7 +55,7 @@ class SchedulerBase(ABC):
     MAXSIZE = maxsize
     ALLOW_MAPPING_SAME_NODE = True
     
-    def __init__(self, _seed, resource_manager, allocator=None, job_check=JobVerification.CHECK_REQUEST, **kwargs):
+    def __init__(self, _seed, resource_manager, allocator=None, job_check=JobVerification.NO_CHECK, **kwargs):
         """
         
         Construct a scheduler
