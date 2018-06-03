c333dba1fb79c94320b77a5c490cb752cca6f4a0
cgalleguillosm <cristian.galleguillos.m@mail.pucv.cl>
2018-06-03 16:05:14 +0200

    Async Writer multiplatform version

--- accasim/utils/async_writer.py
+++ accasim/utils/async_writer.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2017 AlessioNetti
+Copyright (c) 2017 AlessioNetti, cgalleguillosm
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -40,18 +40,13 @@ class AsyncWriter:
         :param pre_process_fun: A pre-processing function for objects pushed to the queue. It MUST be a function that
             receives an object as input, and returns a string representation of it
         """
-        self._path = path
-        self._outfile = None
         self._toTerminate = False
         self._thread = None
         self._deque = deque()
         self._sem = Semaphore(value=0)
         self._buffer_size = buffer_size
-        self._buf_counter = 0
-        if pre_process_fun is None:
-            self._pre_processor = AsyncWriter._dummy_pre_process
-        else:
-            self._pre_processor = pre_process_fun
+        self._buf_counter = 0        
+        self._pre_processor_wrapper = QueueFlusher(path, pre_process_fun)
 
     def push(self, data_obj):
         """
@@ -82,40 +77,31 @@ class AsyncWriter:
             self._thread.join()
             self._thread = None
 
-        if self._outfile is not None:
-            self._outfile.close()
-            self._outfile = None
         self._deque.clear()
 
     def _working_loop(self):
-        self._outfile = open(self._path, 'a')
         while not self._toTerminate or len(self._deque) > 0:
             self._sem.acquire()
-            process = Process(target=self._flush_queue)
+            process = Process(target=self._pre_processor_wrapper.flush, args=(self._deque,))
             process.start()
             process.join()
-            counter_pop = 0
-            while counter_pop < self._buffer_size and len(self._deque) > 0:
-                self._deque.popleft()
-                counter_pop += 1
-        self._outfile.close()
-        self._outfile = None
-
-    def _flush_queue(self):
-        counter_flushed = 0
-        buffer = ''
-        while counter_flushed < self._buffer_size and len(self._deque) > 0:
-            entry = self._deque.popleft()
-            str_out = self._pre_processor(entry)
-            if isinstance(str_out, (list, tuple)) and len(str_out) > 1:
-                for str_el in str_out:
-                    buffer += str_el
-            else:
-                buffer += str_out
-            counter_flushed += 1
-        if len(buffer) > 0:
-            self._outfile.write(buffer)
+            self._deque.clear()
 
     @staticmethod
     def _dummy_pre_process(data_obj):
         return str(data_obj)
+
+class QueueFlusher:
+    
+    def __init__(self, path, func=AsyncWriter._dummy_pre_process):
+        self._path = path
+        self._func = func
+        
+    def flush(self, data, **kwargs):
+        buffer = ''
+        
+        while data:
+            buffer += self._func(data.popleft())
+            
+        with open(self._path, 'a') as f:
+            f.write(buffer)
